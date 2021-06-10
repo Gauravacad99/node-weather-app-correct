772b549cb1887d10e208f7c532b899257ff03662
Gaurav Goyal <Gaurav>
2021-06-10 08:15:43 +0530

    change in output string

--- src/forecast.js
+++ src/forecast.js
@@ -10,7 +10,7 @@ const forecast = function (longitude, latitude, callback) {
             callback('Bad coordinates', undefined)
         }
         else {
-            callback(undefined, `It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+            callback(undefined, `It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out. The humidity is ${response.body.current.humidity}`)
 
         }
     })
